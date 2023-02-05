a1b2e90c67473ddf055e1dc9a99bf368d272c0c9
AdarshMMTQ <91788439+AdarshMMTQ@users.noreply.github.com>
2023-02-05 15:18:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 author =  #change this to your discord id
 token = "" #put your bot token in here
-api = ""
+api = "" #grab it from rapid api [You Chat GPT](https://rapidapi.com/florianbreut/api/you-chat-gpt)
 ext = "chat" #put your bot prefix here
