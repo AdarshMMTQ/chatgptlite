d3b9285625aee0b5c6b852eaaf646c76091b381b
AdarshMMTQ <91788439+AdarshMMTQ@users.noreply.github.com>
2023-02-08 13:29:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 author =  #change this to your discord id
 token = "" #put your bot token in here
 api = "" #check readme.md 
-ext = "chat" #put your bot prefix here
+ext = "chat " #put your bot prefix here
